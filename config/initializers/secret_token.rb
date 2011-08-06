c889a2e17db971a01d8bf469d231c82a4bb86eb7
Stephen Daimler <stephen@packlate.com>
2011-08-06 14:18:42 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bestly::Application.config.secret_token = '9cf1416820d152f27c95caaab3ab26a614691e1111947de103ce68e2f0d4e228367fecc0caa746268c1175dec8c69d21a4af413efb6a885f511fe38122b2e898'
