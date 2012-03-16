5746442b44d20097606126e41bd44f54421ab4c8
Stephen Daimler <stephen@packlate.com>
2012-03-16 09:20:37 -0400

    heroku update hotness fix 2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   def home
     @title = "Home"
     @links = Link.sorted_by_hotness.paginate(:page => params[:page])
-    unless @links.count>30
+    unless Link.count>30
       @links[rand(30)].update_hotness!
     end
   end
