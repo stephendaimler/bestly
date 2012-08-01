136551e6d455802f690a85885def3103bf7a03bd
Stephen Daimler <stephen@packlate.com>
2012-08-01 18:09:30 -0400

    precompiled jquery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,8 @@ Bestly::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  
+  config.assets.precompile += %w( jquery.min.js )
 
   # Generate digests for assets URLs
   config.assets.digest = true
