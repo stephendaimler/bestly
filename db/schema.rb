7dac00c130f059cc470fa62bce58dea71215c340
Stephen Daimler <stephen@packlate.com>
2012-08-02 21:35:45 -0400

    changed url from string to text for longer urls

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,10 +11,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120715231538) do
+ActiveRecord::Schema.define(:version => 20120803012950) do
 
   create_table "links", :force => true do |t|
-    t.string   "url"
+    t.text     "url"
     t.string   "description"
     t.integer  "user_id"
     t.integer  "points",        :default => 0
